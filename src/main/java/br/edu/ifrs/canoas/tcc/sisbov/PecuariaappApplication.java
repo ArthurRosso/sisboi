882f6ce7aaac12b462bdbf6688977d5cde55e6fd
ArthurRosso <arthurderosso@gmail.com>
2018-04-10 17:30:27 +0000

    Primeiro commit

---
+++ src/main/java/br/edu/ifrs/canoas/tcc/sisbov/PecuariaappApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.canoas.tcc.sisbov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuariaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecuariaappApplication.class, args);
+	}
+}
