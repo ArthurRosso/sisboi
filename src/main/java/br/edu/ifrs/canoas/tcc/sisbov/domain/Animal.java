82f4a4d7453ea2c660ecc687a4f5b14490d5ea73
ArthurRosso <arthurderosso@gmail.com>
2018-04-10 17:47:26 +0000

    Adicionar tabela Animal

---
+++ src/main/java/br/edu/ifrs/canoas/tcc/sisbov/domain/Animal.java
@@ -0,0 +1,49 @@
+package br.edu.ifrs.canoas.tcc.sisbov.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Animal implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "increment") 
+	@GenericGenerator( name = "increment", strategy = "increment")
+	private Long cod;
+
+	private String id;
+
+	private Date date;
+
+	public Long getCod() {
+		return cod;
+	}
+
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+﻿
+INSERT INTO FILE (cod, content) VALUES (1,  FILE_READ('./src/main/resources/static/img/face.png'));
+INSERT INTO FILE (cod, content) VALUES (2,  FILE_READ('./src/main/resources/static/img/tio.png'));
+
+INSERT INTO USER (username, active, email, name, password, picture_cod) VALUES ('root', TRUE, 'root@gmail.com', 'Rootson da Silva', 'root', 1);
+INSERT INTO USER (username, active, email, name, password, picture_cod) VALUES ('tio', TRUE, 'tio@gmail.com', 'Usuário teste 001', 'tio', 2);
+
+INSERT INTO ROLE (cod, role) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO ROLE (cod, role) VALUES (2, 'ROLE_USER');
+
+INSERT INTO USER_ROLES (user_username, roles_cod) VALUES ('root', 1);
+INSERT INTO USER_ROLES (user_username, roles_cod) VALUES ('tio', 2);
