b8807e55b58237ba2e908d437d7bc37baf42db4b
ArthurRosso <arthurderosso@gmail.com>
2018-04-10 18:45:01 +0000

    Adicionar tabela Weight

---
+++ src/main/java/br/edu/ifrs/canoas/tcc/sisbov/domain/Weight.java
@@ -0,0 +1,59 @@
+package br.edu.ifrs.canoas.tcc.sisbov.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Weight implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "increment") 
+	@GenericGenerator( name = "increment", strategy = "increment")
+	private Long cod;
+	
+	private float nweight;
+	
+	private String description;
+	
+	private Date date;
+
+	public Long getCod() {
+		return cod;
+	}
+
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+
+	public float getNweight() {
+		return nweight;
+	}
+
+	public void setNweight(float nweight) {
+		this.nweight = nweight;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
