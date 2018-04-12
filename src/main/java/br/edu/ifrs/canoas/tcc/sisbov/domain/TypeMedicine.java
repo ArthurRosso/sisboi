19705751534cde2170ced44c8d9def4acca5ed1c
ArthurRosso <arthurderosso@gmail.com>
2018-04-12 20:06:43 +0000

    Adicionar tabela TypeMedicine

---
+++ src/main/java/br/edu/ifrs/canoas/tcc/sisbov/domain/TypeMedicine.java
@@ -0,0 +1,48 @@
+package br.edu.ifrs.canoas.tcc.sisbov.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class TypeMedicine implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "increment") 
+	@GenericGenerator( name = "increment", strategy = "increment")
+	private Long cod;
+	
+	private String type;
+	
+	private String description;
+
+	public Long getCod() {
+		return cod;
+	}
+
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
