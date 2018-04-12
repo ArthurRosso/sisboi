7629139cbd7f397162779ed1dcb531f8cca3c401
ArthurRosso <arthurderosso@gmail.com>
2018-04-12 15:42:58 +0000

    Adicionar tabela Breed

---
+++ src/main/java/br/edu/ifrs/canoas/tcc/sisbov/domain/Breed.java
@@ -0,0 +1,48 @@
+package br.edu.ifrs.canoas.tcc.sisbov.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Breed implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "increment") 
+	@GenericGenerator( name = "increment", strategy = "increment")
+	private Long cod;
+	
+	private String breed;
+	
+	private String description;
+
+	public Long getCod() {
+		return cod;
+	}
+
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
